4405599b5390b40b1fdd248f561b7bd8f91d323f
dcalsky <dcalsky@163.com>
2016-03-10 20:14:18 +0800

    router structure

---
+++ Readme.md
@@ -0,0 +1,41 @@
+
+# Breakfast of The Tongji University
+
+## Libs
+
+- [x] [Webpack](https://webpack.github.io)
+- [x] [React](https://facebook.github.io/react/)
+- [x] [Redux](https://github.com/reactjs/redux)
+- [x] [Babel](https://babeljs.io/)
+- [x] [Autoprefixer](https://github.com/postcss/autoprefixer)
+- [x] [PostCSS](https://github.com/postcss/postcss)
+- [x] [CSS modules](https://github.com/outpunk/postcss-modules)
+- [x] [Rucksack](http://simplaio.github.io/rucksack/docs)
+- [x] [React Router Redux](https://github.com/reactjs/react-router-redux)
+- [x] [Redux DevTools Extension](https://github.com/zalmoxisus/redux-devtools-extension)
+
+## Setup
+
+```
+$ npm install
+```
+
+## Running
+
+```
+$ npm start
+```
+
+## Build
+
+```
+$ npm run build
+```
+
+## Note
+
+You create a new business website youself by forking this.
+
+# License
+
+MIT
---
+++ client/index.js
@@ -0,0 +1,28 @@
+
+import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import ReactDOM from 'react-dom'
+import React from 'react'
+
+import App from './containers/App'
+import Home from './containers/Home'
+import Order from './containers/Order'
+import Payment from './containers/Payment'
+import configure from './store'
+
+const store = configure()
+const history = syncHistoryWithStore(hashHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App} >
+        <Route path="/order" component={Order} />
+        <Route path="/payment" component={Payment} />
+        <IndexRoute component={Home} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+)
