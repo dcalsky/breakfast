d0fe9c83b463a946465166a198969ee4bab2c016
dcalsky <dcalsky@163.com>
2016-04-03 19:23:43 +0800

    ingredient init

--- client/Api/food.js
+++ client/Api/food.js
@@ -1,13 +1,22 @@
 import AV from 'avoscloud-sdk'
+import { Food } from './init'
 // todo[1]: all mistakes should be added
 
 export const getFoods = (type) => {
-  const query = new AV.Query('Food')
+  let query = new AV.Query('Food')
   query.addDescending('name')
-  //query.equalTo('type', type).limit(10)
   return query.find()
 }
 
 export const getTypes = () => {
   return new AV.Query('Type').find()
+}
+
+export const getIngredient = foodId => {
+  let query = new AV.Query('Ingredient')
+  let food = new Food()
+  food.id = foodId
+  query.addDescending('price')
+  query.equalTo('food', food)
+  return query.find()
 }
--- client/components/FoodList/index.js
+++ client/components/FoodList/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { hashHistory } from 'react-router'
-import ImageLoader from 'react-imageloader';
+import { getIngredient } from '../../Api/food'
+import ImageLoader from 'react-imageloader'
+import Immutable from 'immutable'
 import _ from 'lodash'
 import './style.styl'
 
@@ -11,16 +13,45 @@ const preloader = () => {
 
 // 2016/3/25 remove onClick={this.skipFoodDetail.bind(this, id)} enter detail of food page
 export default React.createClass({
+  getInitialState() {
+    return {
+      isExpandIngredient: false,
+      expandNo: null,
+      ingredients: []
+    }
+  },
   skipFoodDetail(id) {
     hashHistory.push(`/foods/${id}`)
+  },
+  handleExpandIngredient(id) {
+    const isExpandIngredient = id !== this.state.expandNo || !this.state.isExpandIngredient;
+    this.setState({
+      isExpandIngredient: isExpandIngredient,
+      expandNo: id,
+      ingredients: id === this.state.expandNo ? this.state.ingredients : []
+    })
+    if(isExpandIngredient) {
+      if(!this.state[id]) {
+        getIngredient(id).then(ingredients => {
+          const state = Immutable.fromJS(this.state).set(id, ingredients).set("ingredients", ingredients)
+          this.setState(state.toJS())
+        })
+      } else {
+          this.setState({
+            ingredients: this.state[id]
+          })
+      }
+
+    }
   },
   render() {
     return (
       <ul className="food-list">
         {this.props.foods.map((food, i) => {
-          const name = food.get('name'), desc = food.get('desc'), price = food.get('price'), id = food.id, sold = food.get('sold'), img = food.get('image')
+          const id = food.id, name = food.get('name'), desc = food.get('desc'), price = food.get('price'), sold = food.get('sold'), img = food.get('image')
           const _food = _.find(this.props.cart.foods, {id: id})
           const count = _food ? _food.count : 0
+          const isExpand = this.state.isExpandIngredient && this.state.expandNo === id
           return (
             <li key={i} className="food-list-item">
               <div className="food-image">
@@ -41,14 +72,24 @@ export default React.createClass({
 
                 <div className="food-info-price">
                   <i className="fa fa-jpy"></i>
-                  {price}
+                  <span>{price}</span>
                 </div>
-
+                <div className="food-info-ingredient">
+                  <button onClick={this.handleExpandIngredient.bind(this, id)}>
+                    加料
+                    {
+                      isExpand ?
+                        <i className="fa fa-chevron-up"></i>
+                        :
+                        <i className="fa fa-chevron-down"></i>
+                    }
+                  </button>
+                </div>
               </div>
               <div className="food-info-button">
                 {
                   count > 0 ?
-                    <button onClick={this.props.removeFood.bind(this, {id, price, name})}><i className="fa fa-minus"></i></button>
+                    <button className="minus" onClick={this.props.removeFood.bind(this, {id, price, name})}><i className="fa fa-minus"></i></button>
                     :
                     null
                 }
@@ -58,9 +99,23 @@ export default React.createClass({
                     :
                     null
                 }
-                <button onClick={this.props.addFood.bind(this, {id, price, name})}><i className="fa fa-plus"></i></button>
-
+                <button className="plus" onClick={this.props.addFood.bind(this, {id, price, name})}><i className="fa fa-plus"></i></button>
               </div>
+              {
+                isExpand ?
+                  <ul className="ingredient-list">
+                    {this.state.ingredients.map((ingredient, i) => {
+                      return (
+                        <li className="ingredient-iten" key={`ingredient-${i}`}>
+                          {ingredient.get('name')}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                  :
+                  null
+
+              }
             </li>)
         })}
       </ul>
